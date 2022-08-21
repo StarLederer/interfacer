2184738ecd7649525e1f6416746271b1ba182795
Herman Lederer <germans.lederers@gmail.com>
2022-08-21 13:02:00 +0300

    Add more info to website overview

--- packages/renderer/src/lib/Website.svelte
+++ packages/renderer/src/lib/Website.svelte
@@ -1,37 +1,111 @@
 <script>
   import Play from "svelte-material-icons/Play.svelte";
+  import Menu from "svelte-material-icons/DotsVertical.svelte";
   import Edit from "svelte-material-icons/Pencil.svelte";
+  import Favorite from "svelte-material-icons/Star.svelte";
   import { stringToHue } from "./visuals";
   import Button from "../primitives/Button.svelte";
+  import ButtonList from "../primitives/ButtonList.svelte";
   export let label = "";
 </script>
 
-
-
 <article style={`--hue: ${stringToHue(label)}`}>
-  <span class="title">{label}</span>
-  <Button hue={stringToHue(label)} on:click >
-    <Play/>
-  </Button>
-  <Button hue={stringToHue(label)} >
-    <Edit/>
-  </Button>
+  <div class="header">
+    <div class="title">{label}</div>
+    <ButtonList>
+      <Button hue={stringToHue(label)} ghost>
+        <Favorite />
+      </Button>
+      <!-- <Button hue={stringToHue(label)} ghost>
+        <Menu />
+      </Button> -->
+    </ButtonList>
+  </div>
+  <!-- <p>A short description maybe?</p> -->
+  <table>
+    <thead>
+      <tr>
+        <th>Team</th>
+        <th>Last modified</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="team-members">
+          {#each Array(Math.floor(Math.random() * 3 + 1)) as member}
+            <div class="team-member" style={`--hue: ${Math.random() * 360}`} />
+          {/each}
+        </td>
+        <td>Recently</td>
+      </tr>
+    </tbody>
+  </table>
+
+  <ButtonList spread>
+    <Button hue={stringToHue(label)} on:click>
+      Launch
+      <Play />
+    </Button>
+    <Button hue={stringToHue(label)} ghost>
+      <Menu />
+    </Button>
+  </ButtonList>
 </article>
 
-<style>
+<style lang="scss">
   article {
-    display: grid;
-    gap: 0.4rem;
-    grid-template-columns: auto 3rem 3rem;
-    align-items: center;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
 
     background: hsla(var(--hue), 20%, 20%, 20%);
+    --border-width: 1px;
+    border-style: solid;
+    border-width: var(--border-width);
+    border-color: hsla(var(--hue), 20%, 40%, 20%);
     border-radius: 1rem;
-    padding: 0.2rem;
-    padding-inline-start: 1.2rem;
-  }
+    padding: calc(1rem - var(--border-width));
 
-  .title {
-    color: hsl(var(--hue), 20%, 80%);
+    * {
+      --color-l: 60%;
+      --color-hsl: var(--hue), 40%, var(--color-l);
+      --color-a: 80%;
+      --color: hsla(var(--color-hsl), var(--color-a));
+      color: var(--color);
+    }
+
+    .header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    .title {
+      --color-l: 80%;
+      --color-a: 100%;
+      font-weight: 600;
+    }
+
+    table {
+      font-weight: 400;
+    }
+
+    th {
+      width: 50%;
+      --color-a: 40%;
+      text-align: start;
+      font-weight: 400;
+    }
+
+    .team-members {
+      display: flex;
+      // gap: 0.2rem;
+    }
+    .team-member {
+      width: 1.4rem;
+      aspect-ratio: 1/1;
+      background: hsl(var(--hue), 40%, 50%);
+      border-radius: 50%;
+    }
   }
 </style>
