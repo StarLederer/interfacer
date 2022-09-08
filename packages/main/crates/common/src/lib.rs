28401a66230d9617b8c0b4f818e8a67782bbe27e
Herman Lederer <germans.lederers@gmail.com>
2022-09-08 15:37:00 +0300

    Reimplement command interaction separately from the app

--- packages/main/crates/common/src/lib.rs
+++ packages/main/crates/common/src/lib.rs
@@ -1,2 +1,100 @@
+use serde::Serialize;
+use std::{
+    path::Path,
+    process::{Child, Command},
+};
+
 #[cfg(test)]
 mod tests;
+
+#[derive(Default)]
+pub struct Action {
+    pub command: String,
+    pub user_terminated: bool,
+    pub idle_name: String,
+    pub active_name: String,
+    pub process: Option<Child>,
+}
+
+#[derive(Serialize)]
+pub struct Consequence {
+    pub name: String,
+    pub active: bool,
+}
+
+pub fn use_child_process(
+    command: &String,
+    cwd: &Path,
+    await_termination: bool,
+) -> Option<Result<Child, String>> {
+    let mut command_parts = command.split(" ");
+
+    // Create command
+    let mut command = Command::new(&command_parts.next().unwrap());
+    command.current_dir(cwd);
+    for arg in command_parts {
+        // Apply simple preprocess
+        // TODO: Instead find a way to execute Bash and other shell scripting languages
+        command.arg(arg.replace("${pwd}", &cwd.display().to_string()));
+    }
+
+    // Spawn child
+    match command.spawn() {
+        Ok(mut child) => {
+            if await_termination {
+                child.wait().expect("");
+                None
+            } else {
+                Some(Ok(child))
+            }
+        }
+        Err(err) => Some(Err(err.to_string())),
+    }
+}
+
+pub fn interact(action: &mut Action, cwd: &Path) -> Result<Consequence, String> {
+    if action.user_terminated {
+        let name;
+
+        // Check whether to start a new child or stop an existing one
+        if let Some(child) = &mut action.process {
+            // Stop the process
+            match child.kill() {
+                Ok(_) => {
+                    action.process = None;
+                    name = action.idle_name.clone();
+                }
+                Err(err) => {
+                    return Err(err.to_string());
+                }
+            };
+        } else {
+            // Start the child process
+            match use_child_process(&action.command, cwd, false).unwrap() {
+                Ok(child) => {
+                    action.process = Some(child);
+                    name = action.active_name.clone();
+                }
+                Err(err) => return Err(err.to_string()),
+            }
+        }
+
+        Ok(Consequence {
+            name: name,
+            active: action.process.is_some(),
+        })
+    } else {
+        // Error out if action has a running process
+        if action.process.is_some() {
+            return Err("Action already active".to_string());
+        }
+
+        // Execute a child process
+        use_child_process(&action.command, cwd, true);
+
+        Ok(Consequence {
+            name: action.idle_name.clone(),
+            active: false,
+        })
+    }
+}
