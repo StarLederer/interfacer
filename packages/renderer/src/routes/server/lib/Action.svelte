a5c3a335a99eafde6f40ff2fdaa29acceb6727e2
Herman Lederer <germans.lederers@gmail.com>
2022-09-08 15:41:13 +0300

    Makeaction buttons active when loading

--- packages/renderer/src/routes/server/lib/Action.svelte
+++ packages/renderer/src/routes/server/lib/Action.svelte
@@ -14,6 +14,7 @@
     try {
       loading = true;
       error = null;
+      active = true;
       const res = (await invoke("interact", { actionI: i })) as {
         name: string;
         active: boolean;
