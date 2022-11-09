fc6b431142ba1d3a22030b3206b04fa35b6a44d9
Herman Lederer <germans.lederers@gmail.com>
2022-11-09 22:21:46 +0100

    Implement pushing

--- packages/main/crates/common/src/git.rs
+++ packages/main/crates/common/src/git.rs
@@ -25,14 +25,13 @@ pub fn fetch(
     username: &str,
     password: &str,
 ) -> Result<bool, git2::Error> {
-    let mut cb = git2::RemoteCallbacks::new();
-
     // Figure out whether it's a named remote or a URL
     let mut remote = repo
         .find_remote(remote)
         .or_else(|_| repo.remote_anonymous(remote))?;
 
-    // Credentials
+    // Remote callbacks
+    let mut cb = git2::RemoteCallbacks::new();
     cb.credentials(move |_, _, _| git2::Cred::userpass_plaintext(username, password));
 
     let mut fo = git2::FetchOptions::new();
@@ -78,7 +77,11 @@ pub fn nuke_pull(
     let fetch_commit = repo.reference_to_annotated_commit(&fetch_head)?;
 
     // Discard all local changes ("nuke")
-    repo.reset(repo.head()?.peel_to_commit()?.as_object(), git2::ResetType::Hard, None)?;
+    repo.reset(
+        repo.head()?.peel_to_commit()?.as_object(),
+        git2::ResetType::Hard,
+        None,
+    )?;
 
     // Merge (prefer remote changes (should not occur since we discard before merging))
     let mut merge_opts = git2::MergeOptions::new();
@@ -126,4 +129,41 @@ pub fn commit(repo: &git2::Repository) -> Result<git2::Oid, git2::Error> {
         &repo.find_tree(repo.index()?.write_tree()?)?,
         &[&repo.head()?.peel_to_commit()?],
     )
+}
+
+pub fn push(
+    repo: &git2::Repository,
+    remote: &str,
+    username: &str,
+    password: &str,
+) -> Result<(), git2::Error> {
+    // Get remote by given name
+    let mut remote = repo.find_remote(remote)?;
+
+    // Remote callbacks
+    let mut cb = git2::RemoteCallbacks::new();
+    cb.credentials(move |_, _, _| git2::Cred::userpass_plaintext(username, password));
+
+    // Push options
+    let mut po = git2::PushOptions::new();
+    po.remote_callbacks(cb);
+
+    // Iterate over each branch
+    for branch in repo.branches(None)? {
+        let branch = branch?;
+
+        // Continue on every branch that is local and HEAD.
+        // Normally there is only one HEAD in a Git repo
+        if branch.0.is_head() && branch.1 == git2::BranchType::Local {
+            let branch_name = match branch.0.name()? {
+                Some(name) => name,
+                None => return Err(git2::Error::new(git2::ErrorCode::Invalid, git2::ErrorClass::Repository, "Current branch name is not a valid utf-8 string")),
+            };
+
+            // Push
+            remote.push(&[format!("HEAD:refs/heads/{}", branch_name)], Some(&mut po))?;
+        }
+    }
+
+    Ok(())
 }
