cc23f275a3a27e369d8cb92322a17698c61b48ab
Herman Lederer <germans.lederers@gmail.com>
2022-11-10 17:58:44 +0100

    Improve progress colors

--- packages/renderer/src/lib/primitives/Progress.svelte
+++ packages/renderer/src/lib/primitives/Progress.svelte
@@ -9,8 +9,8 @@
 <style lang="postcss">
   div {
     /* TODO: replace --fg with @apply when unocss-preset adds support */
-    border: 0.2rem solid var(--fg0);
-    border-top: 0.2rem solid var(--fg1);
+    border: 0.2rem solid var(--fg2);
+    border-top-color: var(--fg1);
     border-radius: 50%;
     width: calc(var(--radius) * 2);
     height: calc(var(--radius) * 2);
