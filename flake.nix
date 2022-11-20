6a010abad0a201e37faa22dfb0e420403df369e5
Herman Lederer <germans.lederers@gmail.com>
2022-11-20 14:34:20 +0100

    Improve flake

--- flake.nix
+++ flake.nix
@@ -1,57 +1,53 @@
 {
-  description = "Bevy";
+  description = "Interfacer";
 
   inputs = {
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-utils.url = "github:numtide/flake-utils/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, fenix, nixpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    rust = with fenix.packages.${system}; combine [
-      default.cargo
-      default.clippy
-      default.rustc
-      default.rustfmt
-    ];
-  in {
-    devShell.${system} = pkgs.mkShell rec {
-        nativeBuildInputs = with pkgs; [
-          nodejs-16_x
-          nodePackages.pnpm
-
-          rust
-          rust-analyzer
+  outputs = { self, fenix, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        rust = with fenix.packages.${system}; combine [
+          default.cargo
+          default.clippy
+          default.rustc
+          default.rustfmt
         ];
+      in {
+        devShell = pkgs.mkShell rec {
+          nativeBuildInputs = with pkgs; [
+            nodejs-16_x
+            nodePackages.pnpm
 
-        buildInputs = with pkgs; [
-          # These probably can be nativeBuildInputs
-          pkgconfig
-          openssl
-          glib
-          cairo
-          pango
-          atk
-          gdk-pixbuf
-          libsoup
-          gtk3
-          webkitgtk
-          librsvg
-          patchelf
-          dbus
-          SDL2
-        ];
+            rust
+            rust-analyzer
+          ];
 
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
-
-        shellHook = ''
-          cd bevy
-        '';
-      };
-  };
+          buildInputs = with pkgs; [
+            # These probably can be nativeBuildInputs
+            pkgconfig
+            openssl
+            glib
+            cairo
+            pango
+            atk
+            gdk-pixbuf
+            libsoup
+            gtk3
+            webkitgtk
+            librsvg
+            patchelf
+            dbus
+            SDL2
+          ];
+        };
+      }
+    );
 }
