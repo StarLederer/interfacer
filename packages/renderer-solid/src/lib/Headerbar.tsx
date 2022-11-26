6600ec709469b78e4afe8eac223ec52996c73f6d
Herman Lederer <germans.lederers@gmail.com>
2022-11-26 18:06:55 +0100

    Fix typos

--- packages/renderer-solid/src/lib/Headerbar.tsx
+++ packages/renderer-solid/src/lib/Headerbar.tsx
@@ -4,18 +4,18 @@ import Button from "~/lib/primitives/Button";
 type IMainProps = {
   title?: string;
   titleActions?: JSXElement;
-  onBack?: () => {};
+  onBack?: () => void;
 };
 
 const Main: ParentComponent<IMainProps> = (props) => {
   return (
     <div class="flex justify-between items-center pd-m0 gap-m0">
-      <div class="flex items-center flex1 gam-m0">
+      <div class="flex items-center flex1 gap-m0">
         {(props.onBack || props.titleActions) && (
-          <div class="flex items-center ga-s-">
+          <div class="flex items-center gap-s-">
             {props.onBack !== undefined && (
               <Button style="half" onClick={props.onBack}>
-                <div class="i-mdi-chevron-left text-m--" />
+                <div class="i-mdi-chevron-left text-m0" />
               </Button>
             )}
 
